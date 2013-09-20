2c2746e6349b8bf9ddc9b3521d3f6ac06b9ff225
Louis Billiet <louis.billiet59@gmail.com>
2013-09-20 16:09:01 +0200

    Debut de structure pour manger les données du client local

--- tunnel/home.py
+++ tunnel/home.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
+
 import BaseHTTPServer
+from collections import deque
+
+# Utiliser extend et popleft pour le buffer
+buffer = deque()
+local_client_is_up
 
 class HTTP_tunnel_handler(BaseHTTPServer.BaseHTTPRequestHandler):
    def do_GET(s):
@@ -11,14 +18,23 @@ class HTTP_tunnel_handler(BaseHTTPServer.BaseHTTPRequestHandler):
       s.end_headers()
    def do_something(s):
       s.send_response(200)
-      s.send_header("Content-type", "text/html")
+      s.send_header("Content-Type", "application/octet-stream")
+      s.send_header("Content-Transfer-Encoding", "base64")
+      s.send_header("Cache-Control", "no-store")
       s.end_headers()
-      s.wfile.write("<html><head><title>Hey!</title></head><body><h1>What are you doing here?</h1></body></html>")
+      s.wfile.write("Some data")
+
+def eat_local_client_data():
+   # omnomnomnomnom
+   # unset flag
 
 def main():
+   # Ouverture port local
    server_address = ('',8080)
    fresh_server = BaseHTTPServer.HTTPServer(server_address, HTTP_tunnel_handler)
-   fresh_server.serve_forever()
+   # omnomnomnomnom
+   while local_client_is_up:
+      fresh_server.handle_request()
 
 if __name__ == "__main__":
     main()
