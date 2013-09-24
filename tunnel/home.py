e21e027d07c262ff0c6ac40da5683aba2dab8d9c
Louis Billiet <louis.billiet59@gmail.com>
2013-09-24 17:25:35 +0200

    script work scratchée, reste a debugguer

--- tunnel/home.py
+++ tunnel/home.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from time import sleep 
 import base64
 import urlparse
 import socket
@@ -22,6 +23,8 @@ class HTTP_tunnel_handler(BaseHTTPServer.BaseHTTPRequestHandler):
       s.send_header("Content-type", "text/html")
       s.end_headers()
    def do_something(s):
+      global input_buffer
+      global output_buffer
       # Extrais les donnees de la requete
       data = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('data', None)
       input_buffer.extend(data)
@@ -37,16 +40,20 @@ class HTTP_tunnel_handler(BaseHTTPServer.BaseHTTPRequestHandler):
          s.wfile.write("")
 
 def read_local_client_data(connection):
+   global local_client_is_up
+   global output_buffer
    while 1:
-      # On recupere les donnees
+      # On recupere les donnees
       data = connection.recv(1024)
       if not data: break
       output_buffer.extend(base64.b64encode(data))
    local_client_is_up = False
 
 def write_local_client_data(connection):
+   global local_client_is_up
+   global input_buffer
    while 1:
-      # On envoie des donnees si besoin
+      # On envoie des donnees si besoin
       try:
          connection.sendall(base64.b64decode(input_buffer.popleft()))
       except IndexError:
@@ -56,6 +63,7 @@ def write_local_client_data(connection):
       if not local_client_is_up: break
 
 def main():
+   global local_client_is_up
    # Ouverture du socket sur un port aleatoire pour le client local
    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    sock.bind(('',0))
--- tunnel/work.py
+++ tunnel/work.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+from time import sleep 
+import base64
+import urlparse
 import socket
 import threading
 import httplib
+import urllib
 from collections import deque
 
 # Utiliser extend et popleft pour le buffer
@@ -31,47 +35,39 @@ def write_local_client_data(connection):
 
 
 def main():
-	
-
 	# Ouverture du socket sur un port aleatoire pour le client local
-
-	HOST = 'localhost'    # The remote host
-	PORT = int (input("Sur quel port souhaitez-vous vous connecter ?"))             # The same port as used by the server
-	
+	HOST = 'localhost'
+	PORT = int (input("Sur quel port souhaitez-vous vous connecter ?"))
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((HOST, PORT))
-
 	# Ouverture d'un thread qui mangera les donnees du client
 	read_thread = threading.Thread(None, read_local_client_data, None, (s,), {})
 	write_thread = threading.Thread(None, write_local_client_data, None, (s,), {})
-	
-
 	# Init connection HTTP
 	conn = httplib.HTTPConnection("192.168.12.94:8080")
 	headers = {"Content-Type": "application/octet-stream","Content-Transfer-Encoding": "base64", "Cache-Control": "no-store"}
-
-
 	while 1:
 		# modif des data en fonction des event
-		params = urllib.urlencode({'data': output_buffer.popleft()})
-
+		try:
+			params = urllib.urlencode({'data': output_buffer.popleft()})
+		except IndexError:
+			params = urllib.urlencode({'data': ''})
 		# envoie de la request
-		conn.request("POST", "", param, headers)
-		
+		try:
+			conn.request("POST", "", params, headers)
+		except socket.error:
+			sleep(5)
+			continue
 		# reception de la response
 		r1 = conn.getresponse()
-		
 		# test du status  si la responce est ok
 		print r1.status, r1.reason
-
 		data = r1.read()
 		print data
-		
 		# Post dans le buffer le retour du serveur
 		input_buffer.extend(data)
-
 	conn.close()
 	s.close()
 
-
-
+if __name__ == "__main__":
+    main()
