b253b81b662df29eab1dd478219883ab9b02ca76
Louis Billiet <louis.billiet59@gmail.com>
2013-10-08 16:24:20 +0200

    ajout de regles iptables pour le proxy

--- tunnel/launch_proxy.sh
+++ tunnel/launch_proxy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Regles iptables
-rule="POSTROUTING -t nat -s 192.168.122.0/24 ! -d 192.168.122.0/24 -j MASQUERADE"
+rule1="POSTROUTING -t nat -s 192.168.122.0/24 ! -d 192.168.122.0/24 -j MASQUERADE"
+rule2="PREROUTING -t nat -i eth0 -p tcp --dport 3128 -j DNAT --to-destination 192.168.122.10"
+rule3="PREROUTING -t nat -i eth0 -p udp --dport 3128 -j DNAT --to-destination 192.168.122.10"
 
 # Creation d'un pont reseau
 brctl addbr proxybr
@@ -17,14 +19,19 @@ ifconfig $tap up
 
 # Activation de la translation d'adresse
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -A $rule
+iptables -A $rule1
+iptables -A $rule2
+iptables -A $rule3
+
 
 # Lancement de la VM
 kvm -m 512 /vservers/squid.raw -net nic,macaddr=DE:AD:BE:EF:44:40 -net tap,ifname=$tap,script=no
 
 # Desactivation de la translation d'adresse
 echo 0 > /proc/sys/net/ipv4/ip_forward
-iptables -D $rule
+iptables -D $rule1
+iptables -D $rule2
+iptables -D $rule3
 
 # Detachement de l'interface
 brctl delif proxybr $tap
