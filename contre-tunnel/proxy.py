abc3c3d45fb84beb37de15df6094f420a3ed9cbe
Feyd-Aran <loywick@Atheist-Flanders>
2013-10-15 13:57:46 +0200

    Je reglecm les commits!

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+import select
+
+class httpServer(SimpleHTTPRequestHandler):
+	def doPost():
+		
+	def doGet():
+
+def create_http_server(port):
+	pass
+	
 def usage():
     pass
 
 def main():
-    pass
+    create_http_server(port)
 
 if __name__ == '__main__':
-    main()
+	port = sys.argv[1]
+    main(port)
