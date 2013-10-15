26b904aff4a5e69fb2811577299c4beea9893d1b
Louis Billiet <louis.billiet59@gmail.com>
2013-10-15 13:56:51 +0200

    traitement des arguments

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+
 def usage():
-    pass
+    print "proxy.py <port>"
+    exit(1)
 
 def main():
     pass
 
 if __name__ == '__main__':
+    if len(sys.argv)<2:
+        usage()
+    if not sys.argv[1].isdigit():
+        usage()
+    global __port__ = int(sys.argv[1])
     main()
