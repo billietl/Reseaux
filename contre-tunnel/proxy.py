cc9b7a2b1e5f72c8b6ba7af29d76590ecd1d45cf
Feyd-Aran <loywick@Atheist-Flanders>
2013-10-15 14:05:49 +0200

    Plein de trucs

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -2,20 +2,28 @@
 
 import sys, select
 
-class httpServer(SimpleHTTPRequestHandler):
-	def doPost():
-		
-	def doGet():
-
-def create_http_server(port):
-	pass
+class httpRequest(BaseHTTPServer.BaseHTTPRequestHandler):
+   def do_GET(s):
+      httpRequest.do_something(s)
+   def do_POST(s):
+      httpRequest.do_something(s)
+   def do_HEAD(s):
+      s.send_response(200)
+      s.send_header("Content-type", "text/html")
+      s.end_headers()
+   def do_something(s):
+      pass
 
 def usage():
     print "proxy.py <port>"
     exit(1)
 
 def main():
-    create_http_server(port)
+    serveraddress = ('', __port__)
+    proxyserver = BaseHTTPServer.HTTPServer(serveraddress, httpRequest)
+    while True:
+		proxyserver.handle_request()
+	return 0
 
 if __name__ == '__main__':
     if len(sys.argv)<2:
