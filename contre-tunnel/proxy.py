cb4c69dbf3eae2434a01c0c0d26cd7b0b4a9156c
Louis Billiet <louis.billiet59@gmail.com>
2013-10-15 14:15:04 +0200

    ajout de la fonction filter

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -2,6 +2,10 @@
 
 import sys, select
 
+def filter(request):
+   ''' Retourne True si la requete est legit, false si c est un tunnel '''
+   return True
+
 class httpRequest(BaseHTTPServer.BaseHTTPRequestHandler):
    def do_GET(s):
       httpRequest.do_something(s)
@@ -12,7 +16,12 @@ class httpRequest(BaseHTTPServer.BaseHTTPRequestHandler):
       s.send_header("Content-type", "text/html")
       s.end_headers()
    def do_something(s):
-      pass
+      if filter(s):
+         url = s.path
+         headers = s.headers
+      else:
+         s.send_response(400)
+         
 
 def usage():
     print "proxy.py <port>"
@@ -22,7 +31,7 @@ def main():
     serveraddress = ('', __port__)
     proxyserver = BaseHTTPServer.HTTPServer(serveraddress, httpRequest)
     while True:
-		proxyserver.handle_request()
+	proxyserver.handle_request()
 	return 0
 
 if __name__ == '__main__':
