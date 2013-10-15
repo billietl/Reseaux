66e709f5c95e5a3383ab0f9c56e6a1e6649847b0
Feyd-Aran <loywick@Atheist-Flanders>
2013-10-15 16:38:42 +0200

    Il me dit que j'ai changé un truc alors bon

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -66,7 +66,7 @@ def main():
     serveraddress = ('', __port__)
     proxyserver = BaseHTTPServer.HTTPServer(serveraddress, httpRequest)
     while True:
-	proxyserver.handle_request()
+		proxyserver.handle_request()
 	return 0
 
 if __name__ == '__main__':
