b15c0aeb61a4d0a4e4b0fa28653fbf069b9ab2f8
Louis Billiet <louis.billiet59@gmail.com>
2013-10-15 18:02:14 +0200

    nous sommes fourbes

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-import sys, select, urlparse, re, httplib, string
-import BaseHTTPServer
+import re, httplib
 import cherryproxy
 
 class Proxy(cherryproxy.CherryProxy):
    def filter_request(self):
-      pass
+      for _ in xrange(0,5):
+         conn = httplib.HTTPConnection(__proxy__)
+         conn.request(self.req.method, self.req.full_url, self.req.data, self.req.headers)
+         r1 = conn.getresponse()
+         conn.close()
 
    def filter_request_headers(self):
-      pass
+      accepted = True
+      headers = self.req.headers.keys()
+      if not ('user-agent' in headers and ['user-agent'] != None):
+         accepted = False
+      if re.match('192\.168\.\d+\.\d+', self.req.netloc) :
+         accepted = False
+      if not accepted:
+         self.set_response_forbidden(reason="how about no?")
 
    def filter_response(self):
       pass
