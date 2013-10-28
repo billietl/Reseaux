1ab30b43a3094fc9b8fb756baad9662fbc8b4868
Louis Billiet <louis.billiet59@gmail.com>
2013-10-28 17:48:38 +0100

    amelioration du proxy

--- contre-tunnel/proxy.py
+++ contre-tunnel/proxy.py
@@ -5,8 +5,11 @@ import cherryproxy
 
 class Proxy(cherryproxy.CherryProxy):
    def filter_request(self):
+      if not ('content-length' in self.req.headers and len(self.req.data) == int(self.req.headers['content-length'])):
+         print "content-length de mauvaise taille !"
+         self.set_response_forbidden(reason="how about no?")
       for _ in xrange(0,5):
-         conn = httplib.HTTPConnection(__proxy__)
+         conn = httplib.HTTPConnection(self.req.netloc)
          conn.request(self.req.method, self.req.full_url, self.req.data, self.req.headers)
          r1 = conn.getresponse()
          conn.close()
@@ -14,15 +17,19 @@ class Proxy(cherryproxy.CherryProxy):
    def filter_request_headers(self):
       accepted = True
       headers = self.req.headers.keys()
-      if not ('user-agent' in headers and ['user-agent'] != None):
+      if not ('user-agent' in headers):
+         print "User-Agent vide ou incorrect !"
          accepted = False
-      if re.match('192\.168\.\d+\.\d+', self.req.netloc) :
+      if not re.match('.*(\:[80|403])?', self.req.netloc):
+         print "Je suis un proxy web ! Tu m'entends ? WEB !"
          accepted = False
       if not accepted:
          self.set_response_forbidden(reason="how about no?")
 
    def filter_response(self):
-      pass
+      if not self.res.headers['content-encoding'] in self.req.headers['accept-encoding'].split(',').trim():
+         print "Tu sais quoi ? Ton serveur t'as repondu de la merde !"
+         self.set_response_forbidden(reason="how about no?")
 
 if __name__ == '__main__':
     cherryproxy.main(Proxy)
